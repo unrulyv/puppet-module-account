b3fce3969d6212e1ea766b96914523d99324c8a3
Linh Vu <vul@unimelb.edu.au>
2014-09-24 15:30:50 +1000

    validate_re must be a standalone line as it doesn't return any val

--- manifests/locallinuxuser.pp
+++ manifests/locallinuxuser.pp
@@ -13,30 +13,26 @@ define account::locallinuxuser(
   $shell = '/bin/bash',
   $system = false,
 ) {
-  if validate_re($title, '/^[a-z0-9_-]{3,16}$/') {
-    group { $gid:
-      ensure     => $ensure,
-      forcelocal => true,
-      system     => false,
-    }
-    user { $title:
-      ensure           => $ensure,
-      name             => $title,
-      comment          => $fullname,
-      expiry           => $expiry,
-      gid              => $gid,
-      groups           => $groups,
-      home             => $home,
-      managehome       => $managehome,
-      password         => $password,
-      password_max_age => $password_max_age,
-      password_min_age => $password_min_age,
-      shell            => $shell,
-      system           => $system,
-      require          => Group[$gid]
-    }
+  validate_re($title, '/^[a-z0-9_-]{3,16}$/')
+  group { $gid:
+    ensure     => $ensure,
+    forcelocal => true,
+    system     => false,
   }
-  else  {
-    fail("account::locallinuxuser: ${title} is an invalid username")
+  user { $title:
+    ensure           => $ensure,
+    name             => $title,
+    comment          => $fullname,
+    expiry           => $expiry,
+    gid              => $gid,
+    groups           => $groups,
+    home             => $home,
+    managehome       => $managehome,
+    password         => $password,
+    password_max_age => $password_max_age,
+    password_min_age => $password_min_age,
+    shell            => $shell,
+    system           => $system,
+    require          => Group[$gid]
   }
 }
